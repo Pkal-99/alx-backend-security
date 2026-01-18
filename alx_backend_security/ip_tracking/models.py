c3e8c93330264acb6aee8f5bdc11eee02e071f6f
Pkal-99 <kal4ever10q@gmail.com>
2026-01-18 21:11:35 +0300

    BlockedIP

--- alx_backend_security/ip_tracking/models.py
+++ alx_backend_security/ip_tracking/models.py
@@ -20,3 +20,12 @@ class RequestLog(models.Model):
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} @ {self.timestamp}"
+
+class BlockedIP(models.Model):
+    """IP addresses should be blocked."""
+    ip_address = models.GenericIPAddressField(unique=True)
+    reason = models.CharField(max_length=255, blank=True, null=True)
+    blocked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} ({self.reason or 'blocked'})"
